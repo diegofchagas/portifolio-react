876292297d976d5ef5761e9fbb970d47d1b50cb0
Diego Chagas <diegofchagas@gmail.com>
2024-02-14 17:55:48 -0300

    ajustando estrutura e estilizacao do componente works

--- src/components/Works/index.jsx
+++ src/components/Works/index.jsx
@@ -11,51 +11,91 @@ const Works = () => {
     <S.Section id="Work">
      <Title text="Trabalhos"/>
       <S.Projects>
-        <div className="card-project">
+        <S.CardProjects>
           <img src={bike} alt="logo-projeto" />
+          <S.LinkContainer>
           <a className="link" href="https://github.com/diegofchagas/bikcraft" target="blank">
             <GithubLogo size="25"/>
-            
+            github
           </a>
-          <a className="link" href="https://diegofchagas.github.io/bikcraft/" target="blank"> <GoogleChromeLogo size="25"/></a>
-        </div>
-        <div className="card-project">
+          <a className="link" href="https://diegofchagas.github.io/bikcraft/" target="blank"> 
+          <GoogleChromeLogo size="25"/>
+            website
+          </a>
+          </S.LinkContainer>
+        </S.CardProjects>
+
+        <S.CardProjects>
         <img src={todo} alt="logo-projeto" />
+        <S.LinkContainer>
         <a className="link" href="https://github.com/diegofchagas/todo-react/" target="blank">
             <GithubLogo size="25"/>
+            github
           </a>
-          <a className="link" href="https://todo-list-sand-pi.vercel.app/" target="blank"> <GoogleChromeLogo size="25"/></a>
-        </div>
-        <div className="card-project">
-        <img src={tweetLite} alt="" />
-        <a className="link" href="https://github.com/diegofchagas/bikcraft" target="blank">
-            <GithubLogo size="25"/>
+          <a className="link" href="https://todo-list-sand-pi.vercel.app/" target="blank"> 
+          <GoogleChromeLogo size="25"/>
+          webiste
           </a>
-          <a className="link" href="https://diegofchagas.github.io/bikcraft/" target="blank"> <GoogleChromeLogo size="25"/></a>
-        </div>
-        <div className="card-project">
-        <img src={bike} alt=" logo-projeto" />
-        <a className="link" href="https://github.com/diegofchagas/bikcraft" target="blank">
-            <GithubLogo size="25"/>
-          </a>
-          <a className="link" href="https://diegofchagas.github.io/bikcraft/" target="blank"> <GoogleChromeLogo size="25"/></a>
-        </div>
-        <div className="card-project">
-        <img src={todo} alt=" logo-projeto" />
-        <a className="link" href="https://github.com/diegofchagas/bikcraft" target="blank">
-            <GithubLogo size="25"/>
-          </a>
-          <a className="link" href="https://diegofchagas.github.io/bikcraft/" target="blank"> <GoogleChromeLogo size="25"/></a>
-        </div>
-        <div className="card-project">
-        <img src={tweetLite} alt=" logo-projeto" />
-        <a className="link" href="https://github.com/diegofchagas/bikcraft" target="blank">
-            <GithubLogo size="25"/>
-          </a>
-          <a className="link" href="https://diegofchagas.github.io/bikcraft/" target="blank"> <GoogleChromeLogo size="25"/></a>
-        </div>
-      </S.Projects>
+          </S.LinkContainer>
+        </S.CardProjects>
 
+        <S.CardProjects>
+        <img src={tweetLite} alt="" />
+        <S.LinkContainer>
+        <a className="link" href="https://github.com/diegofchagas/bikcraft" target="blank">
+            <GithubLogo size="25"/>
+            github
+          </a>
+          <a className="link" href="https://diegofchagas.github.io/bikcraft/" target="blank"> 
+          <GoogleChromeLogo size="25"/>
+          website
+          </a>
+          </S.LinkContainer>
+        </S.CardProjects>
+
+        <S.CardProjects >
+        <img src={bike} alt=" logo-projeto" />
+        <S.LinkContainer>
+        <a className="link" href="https://github.com/diegofchagas/bikcraft" target="blank">
+            <GithubLogo size="25"/>
+            github
+          </a>
+          <a className="link" href="https://diegofchagas.github.io/bikcraft/" target="blank"> 
+          <GoogleChromeLogo size="25"/>
+          website
+          </a>
+          </S.LinkContainer>
+        </S.CardProjects>
+
+        <S.CardProjects>
+        <img src={todo} alt=" logo-projeto" />
+        <S.LinkContainer>
+          <a className="link" href="https://github.com/diegofchagas/bikcraft" target="blank">
+            <GithubLogo size="25"/>
+            github
+          </a>
+          <a className="link" href="https://diegofchagas.github.io/bikcraft/" target="blank">
+          <GoogleChromeLogo size="25"/>
+          website
+          </a>
+          </S.LinkContainer>
+        </S.CardProjects>
+
+        <S.CardProjects>
+        <img src={tweetLite} alt=" logo-projeto" />
+        <S.LinkContainer>
+        <a className="link" href="https://github.com/diegofchagas/bikcraft" target="blank">
+            <GithubLogo size="25"/>
+            github
+          </a>
+          <a className="link" href="https://diegofchagas.github.io/bikcraft/" target="blank"> 
+          <GoogleChromeLogo size="25"/>
+          website
+          </a>
+          </S.LinkContainer>
+        </S.CardProjects>
+
+      </S.Projects>
     </S.Section>
   )
 }
--- src/components/Works/styles.jsx
+++ src/components/Works/styles.jsx
@@ -6,7 +6,7 @@ export const Section = styled.section`
   text-align: center;
 
   img {
-  width: 300px;
+    width: 300px;
   }
 
   @media (min-width: 768px) {
@@ -21,45 +21,51 @@ export const Projects = styled.div`
   grid-column-gap: 2rem;
   width: calc(100% - 2rem);
   margin-inline: 1rem;
-  row-gap:2rem;
-  
-
-  .card-project{
-  box-shadow: 0 4px 25px rgba(14, 36, 49, .15);
-  border-radius: .5rem;
-  overflow:hidden;
-  
-
-  img{
-    width:320px;
-    height:220px;
-    transition:1s;
-    cursor: pointer;
-  }
-
-  img:hover{
-    transform:scale(1.1)
-  }
-
-  .link{
-    margin-block:.8rem;
-    margin-inline:10px;
-    display: inline-block;
-    background-color: ${({theme})=> theme.colors.primary};
-    color: #fff;
-    padding: .65rem 2.1rem;
-    border-radius: .5rem; 
-  }
-  
-  }
+  row-gap: 2rem;
 
   @media (min-width: 768px) {
-  grid-template-columns: repeat(3,1fr);
-  grid-template-rows: repeat(2,1fr);
-  column-gap: 2rem;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+    column-gap: 2rem;
   }
 
   @media (min-width: 1024px) {
     margin-inline: auto;
+  }
+`;
+
+export const LinkContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 15px;
+
+  .link {
+    display: flex;
+    align-items: center;
+    gap: 2px;
+    margin: 12px 0px;
+
+    background-color: ${({ theme }) => theme.colors.primary};
+    color: #fff;
+    padding: 0.65rem 2.1rem;
+    border-radius: 0.5rem;
+  }
+`;
+
+export const CardProjects = styled.div`
+  box-shadow: 0 4px 25px rgba(14, 36, 49, 0.15);
+  border-radius: 0.5rem;
+  overflow: hidden;
+
+  img {
+    width: 320px;
+    height: 220px;
+    transition: 1s;
+    cursor: pointer;
+  }
+
+  img:hover {
+    transform: scale(1.1);
   }
 `;
